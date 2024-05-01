f460f54ac1c02a48d665a5de5dbbc064c89841f5
Keisuke <igorando11@gmail.com>
2024-05-01 02:55:51 -0300

    Pagina inicial

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,8 @@
+let name = document.getElementById('inputName').value;
+let login = document.getElementById('inputLogin').value;
+let password = document.getElementById('inputPassword').value;
+let usuarioLogado = false;
+
+if (usuarioLogado) {
+
+}
