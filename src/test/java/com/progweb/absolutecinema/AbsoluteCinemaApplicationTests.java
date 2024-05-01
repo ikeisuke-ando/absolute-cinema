43769c21837fdb112c803ba5108aadffd5e2a642
Keisuke <igorando11@gmail.com>
2024-05-01 02:02:10 -0300

    initial commit

---
+++ src/test/java/com/progweb/absolutecinema/AbsoluteCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progweb.absolutecinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbsoluteCinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
