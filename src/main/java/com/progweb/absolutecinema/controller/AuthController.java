9b8eed422a3043383248f2705a10c0fd24ca5f90
Keisuke <igorando11@gmail.com>
2024-05-12 14:26:39 -0300

    imports nao usados

--- src/main/java/com/progweb/absolutecinema/controller/AuthController.java
+++ src/main/java/com/progweb/absolutecinema/controller/AuthController.java
@@ -7,13 +7,9 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
-import javax.swing.text.html.StyleSheet;
-import java.io.IOException;
 
 @Controller
 public class AuthController {
--- src/main/java/com/progweb/absolutecinema/controller/IndexController.java
+++ src/main/java/com/progweb/absolutecinema/controller/IndexController.java
@@ -1,11 +1,8 @@
 package com.progweb.absolutecinema.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.net.Authenticator;
 
 @Controller
 public class IndexController {
