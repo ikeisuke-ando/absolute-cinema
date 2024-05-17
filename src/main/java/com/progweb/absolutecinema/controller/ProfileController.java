60c20eb41f5cde58bab3a18a1e8f3b3a8291cd78
Keisuke <igorando11@gmail.com>
2024-05-17 19:32:19 -0300

    pagina de perfil incompleto

---
+++ src/main/java/com/progweb/absolutecinema/controller/ProfileController.java
@@ -0,0 +1,13 @@
+package com.progweb.absolutecinema.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProfileController {
+
+    @GetMapping("/profile")
+    public String index() {
+        return "profile/index";
+    }
+}
